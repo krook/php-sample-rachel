d7676300a151ee417d5d6f20eeb13b77e51f33db
Daniel Krook <krook@us.ibm.com>
2015-03-14 09:25:07 -0400

    PHP sample app for Rachel.

---
+++ htdocs/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Cloud Foundry demo</title>
+  <meta name="description" content="Cloud Foundry demo">
+  <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+  
+  <div id="img">
+      <img src="http://cloudfoundry.org/images/logo_org.png"/>
+  </div>
+
+  <div>
+    <a href="php-info.php">phpinfo();</a>
+  </div>
+
+  <div>
+    <a href="vcap-info.php">VCAP_SERVICES environment variable</a>
+  </div>
+  
+</body>
+</html>
